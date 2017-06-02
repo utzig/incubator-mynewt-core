bad481a07fd05d0abbafd087aa8ee57d6e98d6aa
Jerzy Kasenberg <jerzy.kasenberg@gmail.com>
2017-06-02 21:15:16 +0200

    DO NOT MERGE: risc-v architecture drop
    bits.h was taken from freedom-sdk
    
    other are not finished yet and most likely be changed and force-pushed.
    
    Context switch may work, other exceptions/interrupts were not tested.

---
+++ kernel/os/src/arch/rv32imac/os_arch_rv32imac.c
@@ -0,0 +1,296 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "os/os.h"
+#include "os_priv.h"
+
+#include <hal/hal_bsp.h>
+#include <hal/hal_os_tick.h>
+#include <mcu/encoding.h>
+
+extern void SysTick_Handler(void);
+
+struct context_switch_frame {
+    uint32_t  pc;
+    /* Callee saved registers */
+    uint32_t  s0;
+    uint32_t  s1;
+    uint32_t  s2;
+    uint32_t  s3;
+    uint32_t  s4; 
+    uint32_t  s5;
+    uint32_t  s6;
+    uint32_t  s7;
+    uint32_t  s8;
+    uint32_t  s9;
+    uint32_t  s10;
+    uint32_t  s11;
+    /* Caller saved register */
+    uint32_t  ra;
+    uint32_t  gp;
+    uint32_t  tp;
+    uint32_t  t0;
+    uint32_t  t1; 
+    uint32_t  t2;
+    uint32_t  t3;
+    uint32_t  t4;
+    uint32_t  t5;
+    uint32_t  t6;
+    uint32_t  a0;
+    uint32_t  a1;
+    uint32_t  a2;
+    uint32_t  a3;
+    uint32_t  a4;
+    uint32_t  a5;
+    uint32_t  a6;
+    uint32_t  a7;
+};
+
+/* XXX: determine how to deal with running un-privileged */
+/* only priv currently supported */
+uint32_t os_flags = OS_RUN_PRIV;
+
+extern struct os_task g_idle_task;
+
+#define OS_TICK_PRIO 0
+
+static int
+os_in_isr(void)
+{
+    // TODO:
+    return 0;
+}
+
+void
+timer_handler(void)
+{
+    os_time_advance(1);
+}
+
+// Try and error section begin
+struct csr
+{
+    uint32_t misa;
+    uint32_t mstatus;
+    uint32_t mie;
+    uint32_t mtvec;
+    uint32_t mcounteren;
+    uint32_t mepc;
+    uint32_t mcause;
+    uint32_t mip;
+    uint32_t mcycle;
+    uint32_t mcycleh;
+} csrs;
+// Try and error section end
+
+void
+os_arch_ctx_sw(struct os_task *t)
+{
+    if ((os_sched_get_current_task() != 0) && (t != 0)) {
+        os_sched_ctx_sw_hook(t);
+    }
+
+// Try and error section begin
+    /* trigger sw interrupt */
+    csrs.mstatus = read_csr(mstatus);
+    csrs.mie = read_csr(mie);
+    csrs.mtvec = read_csr(mtvec);
+    //csrs.mcounteren = read_csr(mcounteren);
+    csrs.mepc = read_csr(mepc);
+    csrs.mip = read_csr(mip);
+    csrs.misa = read_csr(misa);
+    csrs.mcycle = read_csr(mcycle);
+    csrs.mcycleh = read_csr(mcycleh);
+
+// This request software interrupt in case context switch
+// should be done by interrup instead of ECALL
+    //*((uint32_t *)0x02000000) = 1;
+
+    csrs.mip = read_csr(mip);
+// Try and error section end
+    asm("ecall");
+}
+
+os_sr_t
+os_arch_save_sr(void)
+{
+    uint32_t isr_ctx;
+
+    isr_ctx = clear_csr(mstatus, MSTATUS_MIE);
+
+    return isr_ctx;
+}
+
+void
+os_arch_restore_sr(os_sr_t isr_ctx)
+{
+    if (!isr_ctx) {
+        set_csr(mstatus, MSTATUS_MIE);
+    }
+}
+
+int
+os_arch_in_critical(void)
+{
+    return read_csr(mstatus) & MSTATUS_MIE;
+}
+
+/* assumes stack_top will be 8 aligned */
+
+os_stack_t *
+os_arch_task_stack_init(struct os_task *t, os_stack_t *stack_top, int size)
+{
+    struct context_switch_frame *sf;
+    uint32_t *reg;
+
+    /* Get stack frame pointer */
+    sf = (struct context_switch_frame *) ((uint8_t *) stack_top - sizeof(*sf));
+    reg = &sf->a7;
+
+    /* Zero out registers except PC which will be set */
+    while (reg != &sf->pc) {
+        *reg-- = 0;
+    }
+
+    /* Set remaining portions of stack frame */
+    sf->pc = (uint32_t) t->t_func;
+    sf->a0 = (uint32_t) t->t_arg;
+
+    return (os_stack_t *) sf;
+}
+
+void
+os_arch_init(void)
+{
+    os_init_idle_task();
+}
+
+os_error_t
+os_arch_os_init(void)
+{
+    os_error_t err = OS_OK;
+#if 0
+// Code from arm to remain what this section is for
+
+    int i;
+
+    /* Cannot be called within an ISR */
+    err = OS_ERR_IN_ISR;
+    if (__get_IPSR() == 0) {
+        err = OS_OK;
+
+        /* Drop priority for all interrupts */
+        for (i = 0; i < sizeof(NVIC->IP); i++) {
+            NVIC->IP[i] = -1;
+        }
+
+        NVIC_SetVector(SVCall_IRQn, (uint32_t)SVC_Handler);
+        NVIC_SetVector(PendSV_IRQn, (uint32_t)PendSV_Handler);
+        NVIC_SetVector(SysTick_IRQn, (uint32_t)SysTick_Handler);
+
+        /*
+         * Install default interrupt handler, which'll print out system
+         * state at the time of the interrupt, and few other regs which
+         * should help in trying to figure out what went wrong.
+         */
+        NVIC_SetVector(NonMaskableInt_IRQn, (uint32_t)os_default_irq_asm);
+        NVIC_SetVector(HardFault_IRQn, (uint32_t)os_default_irq_asm);
+        NVIC_SetVector(-13, (uint32_t)os_default_irq_asm); /* Hardfault */
+        for (i = 0; i < NVIC_NUM_VECTORS - NVIC_USER_IRQ_OFFSET; i++) {
+            NVIC_SetVector(i, (uint32_t)os_default_irq_asm);
+        }
+
+        /* Set the PendSV interrupt exception priority to the lowest priority */
+        NVIC_SetPriority(PendSV_IRQn, PEND_SV_PRIO);
+
+        /* Set the SVC interrupt to priority 0 (highest configurable) */
+        NVIC_SetPriority(SVCall_IRQn, SVC_PRIO);
+
+        /* Check if privileged or not */
+        if ((__get_CONTROL() & 1) == 0) {
+            os_arch_init();
+        } else {
+            svc_os_arch_init();
+        }
+    }
+#endif
+
+    return err;
+}
+
+uint32_t
+os_arch_start(void)
+{
+    struct os_task *t;
+    struct os_task fake_task;
+
+    /* Get the highest priority ready to run to set the current task */
+    t = os_sched_next_task();
+    /*
+     * First time setup fake os_task struct that only has one pointer for SP
+     * Having that will make context switch function work same for first
+     * and every other time.
+     * This fake SP will be used during initial context switch to store SP
+     * that will never be used.
+     */
+    os_sched_set_current_task(&fake_task);
+
+    /* Intitialize and start system clock timer */
+    os_tick_init(OS_TICKS_PER_SEC, OS_TICK_PRIO);
+
+    /* Mark the OS as started, right before we run our first task */
+    g_os_started = 1;
+
+    /* Perform context switch */
+    os_arch_ctx_sw(t);
+
+    /* This should not be reached */
+    return (uint32_t) (t->t_arg);
+}
+
+os_error_t
+os_arch_os_start(void)
+{
+    os_error_t err;
+
+    err = OS_ERR_IN_ISR;
+    if (os_in_isr() == 0) {
+        err = OS_OK;
+        /* should be in kernel mode here */
+        os_arch_start();
+    }
+
+    return err;
+}
+
+void
+external_interrupt_handler(uintptr_t mcause)
+{
+    // TODO: PLIC code here
+}
+
+void
+timer_interrupt_handler(uintptr_t mcause)
+{
+}
+
+void
+software_interrupt_handler(uintptr_t mcause)
+{
+}
